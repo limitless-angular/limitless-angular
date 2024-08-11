fb3b6283bb0d27acaff06aa0032a1b7ace1ef8bc
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2024-08-11 15:44:25 -0500

    refactor(sanity): remove logic implemented by @sanity/image-url

--- libs/sanity/image-loader/src/loader.ts
+++ libs/sanity/image-loader/src/loader.ts
@@ -13,12 +13,6 @@ interface SanityConfig {
   dataset: string;
 }
 
-interface SanityImageDimensions {
-  width: number;
-  height: number;
-  aspectRatio: number;
-}
-
 const DEFAULT_IMAGE_QUALITY = 75;
 
 function getSanityRefId(image: SanityImageSource): string {
@@ -37,80 +31,47 @@ function getSanityRefId(image: SanityImageSource): string {
   return ref._ref || img._id || '';
 }
 
-function getImageDimensions(id: string): SanityImageDimensions {
-  const dimensions = id.split('-')[2];
-  const [width, height] = dimensions
-    .split('x')
-    .map((num: string) => parseInt(num, 10));
-  const aspectRatio = width / height;
-
-  return { width, height, aspectRatio };
-}
-
-function getCroppedDimensions(
-  image: SanityImageSource,
-  baseDimensions: SanityImageDimensions,
-): SanityImageDimensions {
-  const crop = (image as SanityImageObject).crop;
-
-  if (!crop) {
-    return baseDimensions;
-  }
-
-  const { width, height } = baseDimensions;
-  const croppedWidth = width * (1 - (crop.left + crop.right));
-  const croppedHeight = height * (1 - (crop.top + crop.bottom));
-
-  return {
-    width: croppedWidth,
-    height: croppedHeight,
-    aspectRatio: croppedWidth / croppedHeight,
-  };
-}
-
 export function provideSanityLoader(config: SanityConfig): Provider {
   return {
     provide: IMAGE_LOADER,
     useValue: (loaderConfig: ImageLoaderConfig) => {
-      const { src, width, isPlaceholder, loaderParams } = loaderConfig;
+      const {
+        src,
+        loaderParams = {},
+        width = loaderParams['width'],
+        isPlaceholder,
+      } = loaderConfig;
       const image = src as SanityImageSource;
       const id = getSanityRefId(image);
-
       if (!id) {
         throw new Error('Invalid Sanity image source');
       }
 
-      const originalImageDimensions = getImageDimensions(id);
-      const croppedImageDimensions = getCroppedDimensions(
-        image,
-        originalImageDimensions,
-      );
-
       const builder = imageUrlBuilder(config);
-      let imageBuilder = builder.image(image).auto('format');
+      let imageBuilder = builder
+        .image(image)
+        .auto('format')
+        .fit((loaderParams['fit'] ?? loaderParams['height']) ? 'min' : 'max');
+
+      if (width && loaderParams['height'] && loaderParams['width']) {
+        imageBuilder = imageBuilder.height(
+          Math.round((loaderParams['height'] / loaderParams['width']) * width),
+        );
+      }
 
       if (width) {
         imageBuilder = imageBuilder.width(width);
-        const height = Math.round(width / croppedImageDimensions.aspectRatio);
-        imageBuilder = imageBuilder.height(height);
-      } else {
-        imageBuilder = imageBuilder.width(croppedImageDimensions.width);
-        imageBuilder = imageBuilder.height(croppedImageDimensions.height);
       }
 
-      imageBuilder = imageBuilder.fit('clip');
-
       // Use loaderParams for additional configuration
-      if (loaderParams) {
-        if (loaderParams['quality']) {
-          imageBuilder = imageBuilder.quality(loaderParams['quality']);
-        } else {
-          imageBuilder = imageBuilder.quality(DEFAULT_IMAGE_QUALITY);
-        }
+      if (loaderParams['quality']) {
+        imageBuilder = imageBuilder.quality(loaderParams['quality']);
+      } else {
+        imageBuilder = imageBuilder.quality(DEFAULT_IMAGE_QUALITY);
+      }
 
-        if (loaderParams['blur']) {
-          imageBuilder = imageBuilder.blur(loaderParams['blur']);
-        }
+      if (loaderParams['blur']) {
+        imageBuilder = imageBuilder.blur(loaderParams['blur']);
       }
 
       if (isPlaceholder) {
